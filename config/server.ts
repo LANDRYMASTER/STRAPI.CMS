0c0f4dac59afd6ed8dbd2a9fe97498fd58d0a3e3
LANDRYMASTER <Angebile146@gmail.com>
2025-04-23 17:27:08 +0200

    CORECTION DE L'ERREUR SUR LA KEYS

--- config/server.ts
+++ config/server.ts
@@ -6,10 +6,3 @@ export default ({ env }) => ({
   },
 });
 
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
