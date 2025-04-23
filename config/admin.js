0c781a21104bebd0bfa048a5edf6eedc54c3c175
LANDRYMASTER <Angebile146@gmail.com>
2025-04-23 18:10:21 +0200

    CORECTION DE L'ERREUR SUR LA KEYS

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'uneAutreCleAdminOptionnelle'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
     apiToken: {
       salt: env('API_TOKEN_SALT'),
     },
   });
+  
--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 
 module.exports = ({ env }) => ({
     host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+    port: env.int('PORT', 10000),
     app: {
       keys: env.array('APP_KEYS'),
     },
